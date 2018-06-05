2fc13667bd67f2c93c50387a1e82b784c5c79ebb
renganath_86 <renganath@gmail.com>
2018-06-05 21:28:16 +0530

    Commit

---
+++ MvcApplication1/Controllers/DemoController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+namespace MvcApplication1.Controllers
+{
+    public class DemoController : Controller
+    {
+        //
+        // GET: /Demo/
+
+        public ActionResult Index()
+        {
+            loadnightreqdetailsdropdown();
+            loadpickupdroplst();
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Country()
+        {
+            loadCountry();
+            return View();
+        }
+        
+        public ActionResult Country(string CountryName)
+        {
+            string path = Server.MapPath("~/CountryDet.xml");
+            DataSet ds = new DataSet();
+            ds.ReadXml(path);
+            DataTable dt = new DataTable();
+            dt = ds.Tables[0];
+            DataRow[] dr = dt.Select("Country_Desc='" + CountryName + "'");
+            string Countrycd = string.Empty;
+            if (dr.Length > 0)
+            {
+                Countrycd = dr[0]["Country_Cd"].ToString();
+            }
+            var result = new { Value = Countrycd.ToString() };
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+        public void loadCountry()
+        {
+            List<SelectListItem> lst = new List<SelectListItem>();
+            string path = Server.MapPath("~/CountryDet.xml");
+            DataSet ds = new DataSet();
+            ds.ReadXml(path);
+            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                lst.Add(new SelectListItem { Text = ds.Tables[0].Rows[i]["Country_Desc"].ToString(), Value = ds.Tables[0].Rows[i]["Country_Cd"].ToString() });
+            }            
+            ViewBag.CountryLst = lst;
+        }
+        public ActionResult Unit(string Unit)
+        {
+            string path = Server.MapPath("~/Pincodes.xml");
+            DataSet ds = new DataSet();
+            ds.ReadXml(path);
+            DataTable dt = new DataTable();
+            dt = ds.Tables[0];
+            DataRow[] dr=dt.Select("Name='"+ Unit + "'");
+            string pincd = string.Empty;
+            if (dr.Length > 0)
+            {
+                pincd = dr[0]["Cd"].ToString();
+            }
+            var result = new { Value = pincd.ToString() };
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+        public void loadnightreqdetailsdropdown()
+        {
+            List<SelectListItem> lst = new List<SelectListItem>();
+            lst.Add(new SelectListItem { Text = "Night Drop", Value ="Night Drop" });
+            lst.Add(new SelectListItem { Text = "Others", Value = "Others" });
+            ViewBag.nightdrp = lst;
+            Session["nightdrp"] = lst;
+        }
+        public void loadpickupdroplst()
+        {
+            List<SelectListItem> lst = new List<SelectListItem>();
+            lst.Add(new SelectListItem { Text = "Velachery", Value = "Velachery" });
+            lst.Add(new SelectListItem { Text = "KK Nagar", Value = "KK Nagar" });
+            lst.Add(new SelectListItem { Text = "MKB Nagar", Value = "MKB Nagar" });
+            ViewBag.pickupdrp = lst;
+            Session["nightdrp"] = lst;
+        }
+    }
+}
---
+++ MvcApplication1/Models/Country.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Models
+{
+    public class Country
+    {
+        public List<SelectListItem> Countrylist { get; set; }
+
+        public string CountryCode { get; set; }
+
+        public string Descr { get; set; }
+    }
+}
